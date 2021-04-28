ba4f19e053ffb7a98fe2dcba4f51d5d13e0b9d8b
Logan Anderson <landerson2@upei.ca>
2021-04-28 18:20:28 -0300

    fix: fixed blog index page

--- src/pages/admin/blog/index.tsx
+++ src/pages/admin/blog/index.tsx
@@ -1,4 +1,5 @@
 import { createClient } from '@/util';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import {
   useDocumentCreatorPlugin,
@@ -11,7 +12,6 @@ import {
   PostQueryAllResponseType,
 } from '../../blog';
 
-const client = createClient();
 /**
  * This admin page works in a similar manner to the one found at "pages/admin/index.tsx"
  * The only difference is here we're using a dynamic route variable to fetch the correct file.
@@ -24,50 +24,15 @@ const client = createClient();
 // export const getServerSideProps = BlogPostPage.getStaticProps;
 
 export default function AdminBlogPage() {
-  // const [loading, setLoading] = useState(true);
-  // const [data, setData] = useState<PostQueryAllResponseType>(
-  //   {} as PostQueryAllResponseType,
-  // );
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setLoading(true);
-  //     const data = await client.request<PostQueryAllResponseType>(
-  //       queryAllPosts,
-  //       { variables: {} },
-  //     );
-  //     console.log({ data });
-  //     if (data) {
-  //       setData(data);
-  //     }
-  //   };
-  //   fetchData().then(() => setLoading(false));
-  // }, [setLoading]);
-  // const [payload, isLoading] = useGraphqlForms<PostQueryAllResponseType>({
-  //   query: queryAllPosts,
-  //   variables: {},
-  // });
-
+  const router = useRouter();
   const [payload, isLoading] = useGraphqlForms<PostQueryAllResponseType>({
     query: queryAllPosts,
-    formify: ({ formConfig, createForm, skip }) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      return skip();
-      console.log(formConfig.id);
-      if (formConfig.id === `getPostsDocument`) {
-        console.log({ skip });
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        return skip();
-      }
-
-      return createForm(formConfig);
-    },
     variables: {},
   });
   const BlogPage = getBlogPage(() => {
-    useDocumentCreatorPlugin((res) => console.log(`Created new doc`, res));
+    useDocumentCreatorPlugin((res) =>
+      router.push(`/admin/blog/${res.relativePath.replace(`.md`, ``)}`),
+    );
   });
-  // console.log({ data });
   return isLoading ? <p>Loading...</p> : <BlogPage {...payload} />;
 }
